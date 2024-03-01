42881f547e81308ebc340b377105fb75e12e4c19
linaabtm <161114612+linaabtm@users.noreply.github.com>
2024-03-02 00:52:00 +0400

    Update task 20.py

--- task 20.py
+++ task 20.py
@@ -1,4 +1,19 @@
+def Taylors_row(x):
+    expressions = []
+    counter = 1
+    precision = 8
 
-for n in range (0,inf):
-    ln=((-1)**(n-1)*x**n)/n
-print(ln)
+    while True: 
+        expression = (-1)**(counter-1) * x**counter/ counter
+        counter += 1
+        if abs(expression) < 10**-6:
+            break
+        expressions.append(expression)
+
+    answ = sum(expressions)
+
+    return round(answ, 8)
+
+x = float(input())
+result = Taylors_row(x)
+print(result)
