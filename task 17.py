e32cfc6c64966570e962850cb91efbd9945d265c
linaabtm <161114612+linaabtm@users.noreply.github.com>
2024-03-02 00:50:32 +0400

    Update task 17.py

--- task 17.py
+++ task 17.py
@@ -1,15 +1,29 @@
-letter,number=map(input().split(''))
-countl=0
-countn=0
-if letter==['a','c','e','g']:
-    countl+=1
-if number%2==0:
-    countn+=1
-if countl==0 and countn==0:
-    print('black')
-if countl==1 and countn==0:
-    print('white')
-if countl == 0 and countn == 1:
-    print('white')
-if countl == 1 and countn == 1:
-    print('black')
+def check_color(cell):
+    letters = 'abcdefgh'
+    row, col = cell[0], int(cell[1])
+
+    if (letters.index(row)+1 + col)&1 == 0:
+        return 'black'
+    return 'white'
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ['col\Row', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+def out_space():
+    letters = 'abcdefgh'
+    
+    space = []
+
+    for column in range(0,8):
+        space.append([1,2,3,4,5,6,7,8])
+        for letter in letters:
+            let_ind = letters.index(letter)
+            cell = letter+str(column+1)
+            space[column][let_ind] = f'{letter}{column+1}({let_ind+column+2}{check_color(cell)})'
+    
+    for i, row in enumerate(space[::-1], 1):
+        table.add_row([f'{9-i}', *row])
+
+    print(table)
